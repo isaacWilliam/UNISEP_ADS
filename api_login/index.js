6f1da69489933b4fac5800753fbe3d2a393d934e
Isaac William <izakew19@gmail.com>
2025-09-05 00:34:50 -0300

    Trabalho de api_login

--- api_login/index.js
+++ api_login/index.js
@@ -7,21 +7,27 @@ app.use(express.json());
 
 const listaUsuarios = [];
 
-app.get("/", () => {
+app.get("/", (request, response) => {
 
-    response.status(201).send({msg: "Usuários", listaUsuarios});
+    response.status(200).send(listaUsuarios);
 })
 
 app.get("/login", (request, response) => {
 
-    const usuario = request.body;
+    const usuarioRequest = request.body;
 
-    const usuarioLogin = listaUsuarios.find(u => u.nome === usuario.nome && usuario.username == u.username  && u.senha === usuario.senha);
-    if(!usuarioLogin){
-        response.status(404).send({msg: "Usuário ou senha inválido!!", usuario: usuarioLogin});
+    const usuarioCadastrado = listaUsuarios.find(u =>  usuarioRequest.usuario == u.usuario);
+    if(!usuarioCadastrado){
+        response.status(409).send({msg: "Username não encontrado!!", username: usuarioRequest.usuario});
         return;
     }
-    response.status(201).send({msg: "Login realizado com sucesso!", usuario: usuarioLogin});
+
+    if(usuarioCadastrado.senha != usuarioRequest.senha){
+        response.status(400).send({msg: "Senha inválida!!!", senha: usuarioRequest.senha});
+        return;
+    }
+
+    response.status(200).send({msg: "Login realizado com sucesso!", usuario: usuarioCadastrado});
 })
 
 
@@ -29,10 +35,17 @@ app.post("/cadastro", (request, response) => {
 
     const novoUsuario = request.body;
 
+    const usarioExiste = listaUsuarios.find( u => u.username == novoUsuario.username);
+
+    if(usarioExiste){
+        response.status(409).send({msg: "Username já cadastrado!", username: novoUsuario.username});
+        return;
+    }
+
     listaUsuarios.push({...novoUsuario, id: listaUsuarios.length + 1});
     response.status(201).send({msg: "Usuário cadastrado com sucesso!", usuario: novoUsuario});
 })
 
 app.listen(port, () => {
-    console.log('Rodando pessoas na porta 8080.');
+    console.log('Rodando login na porta 8080.');
 })
