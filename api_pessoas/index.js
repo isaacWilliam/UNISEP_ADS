4ff8dab26a2290a752b500725ea6ed12b22f29ca
Isaac William <izakew19@gmail.com>
2025-09-04 22:18:07 -0300

    Atualização do projeto, put e delete

--- api_pessoas/index.js
+++ api_pessoas/index.js
@@ -5,36 +5,57 @@ const port = 8080;
 
 app.use(express.json());
 
-const listaPessoas = [
-    {id: 0, nome: "Isaac"},
-    {id: 1, nome: "Lucio"},
-    {id: 2, nome: "Joarez"},
-    {id: 3, nome: "Marcos"},
-    {id: 4, nome: "Guilherme"},
-]
-app.get("/pessoas", (request, response) => {
+const listaPessoas = [];
+
+app.get("/", (request, response) => {
 
     const filtro = request.query.filtro;
     console.log(filtro);
 
     let listaFiltrada = listaPessoas;
     if(filtro){
-        listaFiltrada = listaPessoas.filter(pessoa => pessoa.nome.toLocaleLowerCase().includes(filtro));
+        listaFiltrada = listaPessoas.filter(pessoa => pessoa.id == filtro);
     }
 
     response.send(listaFiltrada);
 })
 
 
-app.post("/pessoas/cadastro", (request, response) => {
+app.post("/cadastro", (request, response) => {
 
     const novaPessoa = request.body;
-    console.log(novaPessoa);
 
-
-    listaPessoas.push(novaPessoa);
+    listaPessoas.push({...novaPessoa, id: listaPessoas.length + 1});
     response.status(201).send(novaPessoa);
 })
+
+app.put("/atualizar/:id", (request, response) => {
+    const id = Number(request.params.id);
+    const pessoaAtualizada = request.body;  
+
+    const index = listaPessoas.findIndex(pessoa => pessoa.id == id);
+
+    if(index < 0){
+        response.status(404).send({ mensagem: "Pessoa não encontrada." });
+        return;
+    } 
+
+    listaPessoas[index] = { ...pessoaAtualizada, id: id };
+    response.send(listaPessoas[index]);  
+
+})
+
+app.delete("/deletar/:id", (request, response) => { 
+    const id = request.params.id;
+    const index = listaPessoas.findIndex(pessoa => pessoa.id == id);
+    if(index < 0){
+        response.status(404).send({ mensagem: "Pessoa não encontrada." });
+    } 
+
+    listaPessoas.splice(index, 1);
+    response.status(204).send();    
+
+})  
 
 app.listen(port, () => {
     console.log('Rodando pessoas na porta 8080.');
